6a358514386155c7df74c97d32ad9675bebea5d3
Harshal Patil <harshalpatil@Harshals-MacBook-Air.local>
2025-10-04 20:29:36 +0530

    migrate all issue

--- database/applied_projects.schema.ts
+++ database/applied_projects.schema.ts
@@ -16,7 +16,7 @@ export const seed = async (dropFirst = false) => {
     try {
         if (dropFirst) {
             console.log('Dropping Tables');
-            await DB.schema.dropTableIfExists(APPLIED_PROJECTS);
+            await DB.raw(`DROP TABLE IF EXISTS "${APPLIED_PROJECTS}" CASCADE`);
             console.log('Dropped Tables');
         }
         console.log('Seeding Tables');
