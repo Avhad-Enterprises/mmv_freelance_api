77d42fe5f7a1ab94b5b68fb5b607eeaad07aca18
Avhad-123 <avhadenterprisespc5@gmail.com>
2025-10-11 21:19:07 +0530

    mmv-freelance

---
+++ src/utils/email/index.ts
@@ -0,0 +1,5 @@
+// Main email utility exports
+export { createTransporter, EMAIL_SENDER, APP_NAME } from './emailConfig';
+export { sendInvitationEmail } from './sendInvitationEmail';
+export { sendPasswordResetEmail } from './sendPasswordResetEmail';
+export { sendSupportTicketEmail } from './sendSupportTicketEmail';
